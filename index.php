249216a5fb19001cdf8009b15e671b55f6c11ce9
mateoPuntoJar <manuelmtfz@gmail.com>
2024-10-28 17:39:16 +0100

    Uploading the project

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types = 1);?>
+
+<?php
+    const API_URL = "https://whenisthenextmcufilm.com/api";
+    // Inicializar una nueva sesión de cURL; ch = cURL handle
+    $ch = curl_init(API_URL);
+    // Indicar que queremos recibir el resultado de la petición y no mostrarla en pantalla
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // Ejecutar la petición y guardamos el resultado.
+    $result = curl_exec($ch);
+    $data = json_decode($result, true);
+    curl_close($ch);
+?>
+                                                                                                                                                                     
+<head>
+    <title>La próxima película de Marvel</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="La próxima película de Marvel">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
+</head>
+
+<main>
+    <section>
+        <img src="<?= $data["poster_url"]; ?>" alt="Poster de <?= $data["title"]; ?>" width= "500" height="400"
+        style="border-radius: 16px"/>
+    </section>
+
+    <hgroup>
+        <h3><?= $data["title"]; ?> se estrena en <?= $data["days_until"]; ?> días</h3>
+        <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
+        <p>La siguiente película es: <?= $data["following_production"]["title"]; ?></p>
+    </hgroup>
+</main>
+
+<style>
+    :root{
+        color-scheme: light dark;
+    }
+
+    body{
+        display: grid;
+        place-content: center;
+    }
+
+    section{
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+
+    hgroup{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+
+    img{
+        margin: 0 auto;
+    }
+</style>
